980013d2739554ebd633d4f1659f7a20733864cb
Spencer Mitchell <smitch.work@gmail.com>
2018-01-18 22:19:08 -0500

    Update Root test

--- src/__tests__/App.tsx
+++ src/__tests__/App.tsx
@@ -2,19 +2,15 @@ import * as React from 'react';
 import { Container } from 'native-base';
 import { shallow, configure, mount, render } from 'enzyme';
 
-// import Root from '../index';x`
-// import Adapter from 'enzyme-adapter-react-16';
+import Root from '../index';
+import Adapter from 'enzyme-adapter-react-16';
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
-// describe('<Root />', () => {
-//   it('renders container correctly', () => {
-//     const wrapper: any = shallow(<Root />);
-//     expect(wrapper.render().find(Container)).toHaveLength(1);
-//   });
-// });
-
-it('works', () => {
-
+describe('<Root />', () => {
+  it('renders container correctly', () => {
+    const wrapper: any = shallow(<Root />);
+    expect(wrapper).toHaveLength(1);
+  });
 });
 
