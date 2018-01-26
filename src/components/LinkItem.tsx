286b88ea2bad4ea1d305779164c5c6bcbb2da3f9
Spencer Mitchell <smitch.work@gmail.com>
2018-01-25 22:13:16 -0500

    Add score, author, subreddit, commentCount, and domain data

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -6,14 +6,23 @@ interface props {
   post: any;
 }
 
-const LinkItem: React.SFC<props> = post => (
+const LinkItem: React.SFC<props> = ({ post }) => (
   <ListItem>
     <Body style={styles.postBody} >
-      <Text> { post.post.title } </Text>
+      <Text style={styles.postHeader}>
+        <Text style={styles.score}>{ post.score }</Text>
+        <Text>{ ' ' + post.author }</Text>
+        <Text> in </Text>
+        <Text>{ post.subreddit }</Text>
+      </Text>
+      <Text style={styles.title}>{ post.title }</Text>
+      <Text style={styles.postHeader}>
+        { `${post.commentCount} comments | ${post.domain}` }
+      </Text>
     </Body>
     {
-      post.post.thumbnail &&
-      <Thumbnail square size={80} source={{ uri: post.post.thumbnail }}/>
+      post.thumbnail &&
+      <Thumbnail square size={80} source={{ uri: post.thumbnail }}/>
     }
   </ListItem>
 );
@@ -21,6 +30,16 @@ const LinkItem: React.SFC<props> = post => (
 const styles = StyleSheet.create({
   postBody: {
     padding: 0,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  postHeader: {
+    fontSize: 12,
+  },
+  score: {
+    fontSize: 16,
   },
 });
 
